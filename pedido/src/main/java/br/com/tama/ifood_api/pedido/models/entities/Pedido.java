d36699f99338c650946b0af1bbb9e7fc99e8cbe5
Wilson Ferreira <wilson_tama@hotmail.com>
2021-08-19 18:15:26 -0300

    feat(Pedido): Adicionando PanacheMontoEntity na entidade de Pedido

--- pedido/src/main/java/br/com/tama/ifood_api/pedido/models/entities/Pedido.java
+++ pedido/src/main/java/br/com/tama/ifood_api/pedido/models/entities/Pedido.java
@@ -1,11 +1,12 @@
 package br.com.tama.ifood_api.pedido.models.entities;
 
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 
 import java.util.List;
 
 @MongoEntity(collection = "pedido", database = "pedido")
-public class Pedido {
+public class Pedido  extends PanacheMongoEntity {
 
     public String cliente;
 
